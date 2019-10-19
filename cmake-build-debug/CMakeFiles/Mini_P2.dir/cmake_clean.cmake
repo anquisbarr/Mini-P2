d11af5e8c4f089ca88db59f9c59247ae8ac42941
Sebastian Q.B <sebastian.quispe.b@utec.edu.pe>
2019-10-19 13:47:00 -0500

    Adding files

---
+++ cmake-build-debug/CMakeFiles/Mini_P2.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Mini_P2.dir/Country.cpp.obj"
+  "CMakeFiles/Mini_P2.dir/main.cpp.obj"
+  "Mini_P2.exe"
+  "Mini_P2.exe.manifest"
+  "Mini_P2.pdb"
+  "libMini_P2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Mini_P2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
